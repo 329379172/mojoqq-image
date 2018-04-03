2abcffb485b4e774a7319a7a8e869588a879fb0f
linfeiyang <lupengfei@gozap.com>
2018-04-03 21:43:49 +0800

    添加arm版本

---
+++ arm64v8/main.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use lib "../lib/";
+use Mojo::Webqq;
+use Digest::MD5 qw(md5_hex);
+my $client=Mojo::Webqq->new(
+		account     => $ENV{QQ},
+		pwd         => md5_hex($ENV{pwd}),  
+		http_debug  =>  0,
+		log_level   =>  "debug",
+		login_type  =>  "login"
+);
+$client->on(receive_message=>sub{
+				my ($client,$msg)=@_;
+		});
+$client->load("ShowMsg");
+$client->load("Openqq",data=>{post_api=>$ENV{POST_API}});
+$client->run();
+
